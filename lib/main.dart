8c381f2dce3eb4e2f6098eacd48ceb816447b4df
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-10-14 15:13:52 -0300

    adicionando botoes em columns

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,23 @@ main() {
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Home flutter'));
+    final perguntas = [
+      'Qual sua cor favorita?',
+      'Qual seu animal favorito',
+    ];
+
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('AppBar'),
+            ),
+            body: Column(
+              children: [
+                Text(perguntas[0]),
+                ElevatedButton(onPressed: null, child: Text('Click aqui')),
+                TextButton(onPressed: null, child: Text('TextButton')),
+                OutlinedButton(onPressed: null, child: Text('outlined Button'))
+              ],
+            )));
   }
 }
